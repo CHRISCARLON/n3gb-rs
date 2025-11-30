d5f85de44ab346cb6555e664e168d6191203266a
Chris Carlon <cmcarleone@gmail.com>
2025-11-30 16:35:44 +0000

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use n3gb_rs::{HexCell, N3gbError};
+use n3gb_rs::{HexCell, N3gbError, decode_hex_identifier};
 
 fn main() -> Result<(), N3gbError> {
     let lon = -2.2479699500757597;
@@ -11,10 +11,14 @@ fn main() -> Result<(), N3gbError> {
     println!("Row: {}, Col: {}", cell.row, cell.col);
 
     let polygon = cell.to_polygon();
-    println!("Polygon from Poly: {:?}", polygon);
+    println!("Polygon: {:?}", polygon);
 
-    let polygon = cell.to_polygon();
-    println!("Polygon from Poly: {:?}", polygon);
+    let (version, easting, northing, zoom_level) = decode_hex_identifier(&cell.id)?;
+    println!("\nDecoded from ID:");
+    println!("  Version: {}", version);
+    println!("  Easting: {}", easting);
+    println!("  Northing: {}", northing);
+    println!("  Zoom: {}", zoom_level);
 
     Ok(())
 }
